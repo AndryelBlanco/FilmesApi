2c5bab53f2275b6ca64824fd03761f2e7e4ac05a
Andryel Blanco <andryelmblanco@gmail.com>
2022-10-01 23:02:02 -0300

    Creating first method

---
+++ FilmesApi/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase   
+    {
+        private static List<MovieData> ListOfMovies = new List<MovieData>();
+
+
+        //Padrão de verbo
+        [HttpPost]
+        public void addMovie([FromBody] MovieData movie) //FromBody diz que o movie vem do corpo da requisição
+        {
+            ListOfMovies.Add(movie);
+        }
+    }
+}
---
+++ FilmesApi/Models/MovieData.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class MovieData
+    {
+        [Required(ErrorMessage = "Movie Title cannot be null")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Movie Director cannot be null")]
+        public string Director { get; set; }
+
+        public string Genre { get; set; }
+
+        [Range(1, 600)]
+        public int Duration { get; set; }
+
+    }
+}
